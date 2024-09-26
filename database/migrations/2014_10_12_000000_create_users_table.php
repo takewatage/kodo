defb5bcf2034e2a09a2b5ef6e0331f7b8380adf1
ishizuka takeaki <>
2024-09-26 14:48:07 +0900

    fix user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('urlName')->unique()->comment('@から始まるname');
+            $table->string('url_name')->unique()->comment('@から始まるname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
